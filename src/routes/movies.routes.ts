44ff70dda7f4a86d412ff8ecb86d1701e57ae04a
fabiodelllima <f.f.fabiodelima@gmail.com>
2023-09-30 08:15:26 -0300

    feat: Middleware para validar nome de filme

--- src/routes/movies.routes.ts
+++ src/routes/movies.routes.ts
@@ -6,10 +6,11 @@ import {
   readOneMovie,
 } from '../logic';
 import { isMovieIdValid } from '../middlewares/isMovieIdValid';
+import { isMovieNameRegistered } from '../middlewares/isMovieNameRegistered';
 
 export const moviesRoutes = Router();
 
 moviesRoutes.get('/', readMovies);
 moviesRoutes.get('/:id', isMovieIdValid, readOneMovie);
-moviesRoutes.post('/', createMovie);
+moviesRoutes.post('/', isMovieNameRegistered, createMovie);
 moviesRoutes.delete('/:id', isMovieIdValid, deleteMovie);
