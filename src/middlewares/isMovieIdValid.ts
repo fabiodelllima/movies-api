896a05b7c55a223902e0e6acb10cf964adcc500d
fabiodelllima <f.f.fabiodelima@gmail.com>
2023-09-30 07:56:38 -0300

    feat: Middleware para validar ID

---
+++ src/middlewares/isMovieIdValid.ts
@@ -0,0 +1,22 @@
+import { NextFunction, Request, Response } from 'express';
+import format from 'pg-format';
+import { client } from '../database';
+
+export const isMovieIdValid = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const query = format(
+    `SELECT * FROM movies WHERE id = %L;`,
+    req.params.id
+  );
+
+  const data = await client.query(query);
+
+  if (!data.rows[0]) {
+    return res.status(404).json({ message: 'Movie not found.' });
+  }
+
+  next();
+};
--- src/routes/movies.routes.ts
+++ src/routes/movies.routes.ts
@@ -5,10 +5,11 @@ import {
   readMovies,
   readOneMovie,
 } from '../logic';
+import { isMovieIdValid } from '../middlewares/isMovieIdValid';
 
 export const moviesRoutes = Router();
 
 moviesRoutes.get('/', readMovies);
-moviesRoutes.get('/:id', readOneMovie);
+moviesRoutes.get('/:id', isMovieIdValid, readOneMovie);
 moviesRoutes.post('/', createMovie);
-moviesRoutes.delete('/:id', deleteMovie);
+moviesRoutes.delete('/:id', isMovieIdValid, deleteMovie);
